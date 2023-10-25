eaf1c8cd6f7211f40d52fdd677a960d5716182d9
bellil02 <fatma.bellil@esprit.tn>
2023-10-25 20:34:03 +0100

    save&delete

--- app/src/main/java/com/jmaaix/testttttt/MyNoteF.java
+++ app/src/main/java/com/jmaaix/testttttt/MyNoteF.java
@@ -1,64 +1,104 @@
 package com.jmaaix.testttttt;
 
 import android.os.Bundle;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyNoteF#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyNoteF extends Fragment {
+    private LinearLayout notesContainer;
+    private List<Note> noteList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyNoteF() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyNoteF.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyNoteF newInstance(String param1, String param2) {
-        MyNoteF fragment = new MyNoteF();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View rootView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_my_note, container, false);
+
+        notesContainer = rootView.findViewById(R.id.notesContainer);
+        Button saveButton = rootView.findViewById(R.id.saveButton);
+        noteList = new ArrayList<>();
+
+        saveButton.setOnClickListener(v -> saveNote());
+
+        displayNotes();
+
+        return rootView;
+    }
+
+    private void displayNotes() {
+        for (Note note : noteList) {
+            createNoteView(note);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_note, container, false);
+    //enregistrer une nouvelle note dans une liste de notes
+    // en utilisant les données entrées par l'utilisateur dans des champs de texte
+    private void saveNote() {
+        EditText titleEditText = rootView.findViewById(R.id.titleEditText);
+        EditText contentEditText = rootView.findViewById(R.id.contentEditText);
+        String title = titleEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        if (!title.isEmpty() && !content.isEmpty()) {
+            Note note = new Note();
+            note.setTitle(title);
+            note.setContent(content);
+
+            noteList.add(note);
+            createNoteView(note);
+            clearInputFields();
+        }
     }
-}
+    private void clearInputFields() {
+        EditText titleEditText = rootView.findViewById(R.id.titleEditText);
+        EditText contentEditText = rootView.findViewById(R.id.contentEditText);
+
+        titleEditText.getText().clear();
+        contentEditText.getText().clear();
+    }
+
+    //creation et affichage d'une note View dans l'interface utilisateur
+    private void createNoteView(final Note note) {
+        View noteView = getLayoutInflater().inflate(R.layout.note_item, notesContainer, false);
+        TextView titleTextView = noteView.findViewById(R.id.titleTextView);
+        TextView contentTextView = noteView.findViewById(R.id.contentTextView);
+        titleTextView.setText(note.getTitle());
+        contentTextView.setText(note.getContent());
+
+        noteView.setOnLongClickListener(v -> { showDeleteDialog(note);
+            return true;
+        });
+        notesContainer.addView(noteView);
+    }
+
+    //gérer la suppression d'une note
+    private void showDeleteDialog(final Note note) {
+        if (getActivity() != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Delete this note.");
+            builder.setMessage("Are you sure you want to delete this note?");
+
+            builder.setPositiveButton("Delete", (dialog, which) -> deleteNoteAndRefresh(note));
+
+            builder.setNegativeButton("Cancel", null);
+            builder.show();
+        }
+    }
+        private void deleteNoteAndRefresh (Note note){
+            noteList.remove(note);
+            refreshNoteView();
+        }
+        private void refreshNoteView () {
+            notesContainer.removeAllViews();
+            displayNotes();
+        }
+    }
---
+++ app/src/main/java/com/jmaaix/testttttt/Note.java
@@ -0,0 +1,35 @@
+package com.jmaaix.testttttt;
+
+public class Note {
+    private String title;
+    private String content;
+
+    public Note()
+    {}
+
+    public String getTitle(){
+
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title =title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+    public void setContent (String content)
+    {
+        this.content = content;
+
+    }
+
+    public Note(String title, String content){
+        this.title=title;
+        this.content =content;
+
+    }
+
+}
