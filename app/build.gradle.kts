162f89bf64ed7ee407549d2fba60b895ca1ce7bf
bellil02 <fatma.bellil@esprit.tn>
2023-10-30 14:19:37 +0100

    record

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.biometric:biometric:1.1.0")
+    implementation ("androidx.activity:activity-ktx:1.3.0")
 
 }
--- app/src/main/java/com/jmaaix/testttttt/MyNoteF.java
+++ app/src/main/java/com/jmaaix/testttttt/MyNoteF.java
@@ -1,8 +1,15 @@
 package com.jmaaix.testttttt;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,25 +17,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyNoteF extends Fragment {
+
+    private ActivityResultLauncher<Intent> voiceRecognitionLauncher;
+
     private LinearLayout notesContainer;
     private List<Note> noteList;
 
     private View rootView;
 
+    private static final int REQUEST_CODE_VOICE_RECOGNITION = 100;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_my_note, container, false);
 
         notesContainer = rootView.findViewById(R.id.notesContainer);
         Button saveButton = rootView.findViewById(R.id.saveButton);
+        Button recordVoiceNoteButton = rootView.findViewById(R.id.recordVoiceNoteButton);
         noteList = new ArrayList<>();
 
         saveButton.setOnClickListener(v -> saveNote());
-
+        recordVoiceNoteButton.setOnClickListener(v -> startVoiceRecognition());
         displayNotes();
 
         return rootView;
@@ -38,6 +52,61 @@ public class MyNoteF extends Fragment {
         for (Note note : noteList) {
             createNoteView(note);
         }
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Créez le gestionnaire pour la reconnaissance vocale en utilisant ActivityResultContracts
+        voiceRecognitionLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        ArrayList<String> voiceResults = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        if (voiceResults != null && !voiceResults.isEmpty()) {
+                            String voiceNote = voiceResults.get(0);
+                            saveVoiceNoteAndDisplay(voiceNote);
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_VOICE_RECOGNITION && resultCode == Activity.RESULT_OK) {
+            ArrayList<String> voiceResults = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (voiceResults != null && !voiceResults.isEmpty()) {
+                String voiceNote = voiceResults.get(0);
+                saveVoiceNoteAndDisplay(voiceNote);
+            } else {
+                showToast("Aucun résultat vocal valide trouvé. Veuillez réessayer.");
+            }
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void saveVoiceNoteAndDisplay(String voiceNote) {
+        if (!voiceNote.isEmpty()) {
+            Note note = new Note();
+            note.setContent(voiceNote);
+
+            noteList.add(note);
+            createNoteView(note);
+        }
+    }
+
+    private void startVoiceRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Parlez pour enregistrer votre note...");
+        voiceRecognitionLauncher.launch(intent);
     }
 
     //enregistrer une nouvelle note dans une liste de notes
